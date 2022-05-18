7fddb318fe7cd82a78b2e7aa55fbb2d59984d9d6
summus2020 <artweb.info@gmail.com>
2022-05-18 11:52:58 +0300

    update FirstViewConrtoller

--- SearchRepo/FirstViewController.swift
+++ SearchRepo/FirstViewController.swift
@@ -8,13 +8,95 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+public protocol FirstViewControllerDelegate: class {
+    func navigateToNextPage()
 }
 
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+    public weak var delegate:FirstViewControllerDelegate?
+    let tableView = UITableView()
+    var data:[AnyObject] = []
+    var lbl_search:UILabel!
+    var searchBar:UISearchBar!
+    var lbl_repo:UILabel!
+    var safeArea: UILayoutGuide!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.white
+        safeArea = self.view.layoutMarginsGuide
+        
+        lbl_search = UILabel(frame: CGRect(origin: CGPoint(x: 10, y: 0), size: CGSize(width: UIScreen.main.bounds.size.width, height: 30)))
+        lbl_search.text = "Search"
+        lbl_search.textColor = UIColor.black
+        lbl_search.backgroundColor = UIColor.clear
+        lbl_search.font = UIFont.boldSystemFont(ofSize: 28)
+        lbl_search.sizeToFit()
+        self.view.addSubview(lbl_search)
+        
+        searchBar = UISearchBar(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: UIScreen.main.bounds.size.width, height: 60)))
+        searchBar.searchBarStyle = UISearchBar.Style.minimal
+        self.view.addSubview(searchBar)
+        searchBar.delegate = self
+        
+        lbl_repo = UILabel(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 30))
+        lbl_repo.text = "Repositories"
+        lbl_repo.textColor = UIColor.black
+        lbl_repo.backgroundColor = UIColor.clear
+        lbl_repo.font = UIFont.boldSystemFont(ofSize: 22)
+        lbl_repo.sizeToFit()
+        self.view.addSubview(lbl_repo)
+        
+        self.view.addSubview(self.tableView)
+        self.setupConstraints()
+        
+    }
+    
+    
+    func setupConstraints(){
+        // constraints for label "Search"
+        lbl_search.translatesAutoresizingMaskIntoConstraints = false
+        lbl_search.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 28).isActive = true
+        lbl_search.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 0).isActive = true
+        
+        // constraints for search bar
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.topAnchor.constraint(equalTo: lbl_search.bottomAnchor, constant: 4).isActive = true
+        searchBar.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 4).isActive = true
+        searchBar.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -4).isActive = true
+        
+        // constraints for label "Repositories"
+        lbl_repo.translatesAutoresizingMaskIntoConstraints = false
+        lbl_repo.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 16).isActive = true
+        lbl_repo.leftAnchor.constraint(equalTo: lbl_search.leftAnchor).isActive = true
+        
+        // constraints for tableView
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: lbl_repo.bottomAnchor, constant: 10).isActive=true
+        tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 0).isActive=true
+        tableView.leftAnchor.constraint(equalTo: lbl_repo.leftAnchor, constant: 0).isActive = true
+        tableView.rightAnchor.constraint(equalTo: safeArea.rightAnchor, constant: 0).isActive = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0{
+            return 1
+        }
+        else{
+            return data.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+        
+        return cell
+    }
+    
+    func goToSecondPage(){
+        self.delegate?.navigateToNextPage()
+    }
+    
+}
