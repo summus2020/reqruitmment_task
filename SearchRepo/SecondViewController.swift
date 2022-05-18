02d90aa9d47195dd1d57d6509d14468cb41fe73f
summus2020 <artweb.info@gmail.com>
2022-05-19 02:33:40 +0300

    button View Online added

--- SearchRepo/SecondViewController.swift
+++ SearchRepo/SecondViewController.swift
@@ -26,6 +26,18 @@ class SecondViewController: UIViewController {
         return imgView
     }()
     
+    var but_online:UIButton = {
+        let but = UIButton(type: UIButton.ButtonType.system)
+        but.backgroundColor = UIColor.init(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+        //but.titleLabel?.text = "VIEW ONLINE"
+        but.setTitle("VIEW ONLINE", for: .normal)
+        but.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        but.layer.cornerRadius = 19
+        but.clipsToBounds = true
+        but.translatesAutoresizingMaskIntoConstraints = false
+        return but
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         
@@ -33,6 +45,9 @@ class SecondViewController: UIViewController {
         
         view.addSubview(avatarImageView)
         avatarImageView.downloaded(from: repo.avatarLink, contentMode: .scaleAspectFill)
+        
+        view.addSubview(but_online)
+        but_online.addTarget(self, action: #selector(on_but_online_clicked), for: UIControl.Event.touchDown)
         
         setupConstraints()
     }
@@ -48,11 +63,30 @@ class SecondViewController: UIViewController {
             avatarImageView.topAnchor.constraint(equalTo: view.topAnchor),
             avatarImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             avatarImageView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            avatarImageView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: h)
+            avatarImageView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: h),
+            
+            but_online.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 20),
+            but_online.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            but_online.widthAnchor.constraint(equalToConstant: 130),
+            but_online.heightAnchor.constraint(equalToConstant: 38)
         ])
     }
     
     @objc func navigateToFirstPage(){
         self.delegate?.navigateToFirstPage()
+    }
+    
+    // Button "View Online" clicked
+    @objc func on_but_online_clicked(){
+        // animate background color
+        UIView.animate(withDuration: 0.25, animations: {
+            self.but_online.backgroundColor = UIColor.lightGray
+        }) { (complete) in
+            UIView.animate(withDuration: 0.25, animations: {
+                self.but_online.backgroundColor = UIColor.init(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+            }, completion: { (complete) in
+                print("button pressed")
+            })
+        }
     }
 }
